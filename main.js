bcefe2338012492e02b1a41046a3fb634122a9db
Alibek Tauirbaev <featternotving@gmail.com>
2024-11-17 23:52:05 +0600

    first practice JavaScript

---
+++ main.js
@@ -0,0 +1,32 @@
+let userName = ("Алибек");
+let age = ("26");
+let stadt = ("Караганда");
+
+console.log(userName +  age + stadt);
+
+
+
+console.log(typeof(userName));
+
+console.log(typeof(age));
+
+console.log(typeof(stadt));
+
+
+
+let convertedAge = Number(prompt("Введите ваш возраст"));
+
+console.log(convertedAge);
+
+
+let num = Number(prompt("Введите первое число"));
+
+let num1 = Number(prompt("Введите второе число"));
+
+console.log("Сумма чисел=", num + num1);
+
+console.log("Разность чисел =", num - num1);
+
+console.log("Произведение чисел=", num * num1);
+
+console.log("Частное чисел=", num / num1);
